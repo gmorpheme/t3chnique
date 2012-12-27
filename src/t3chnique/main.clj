619e2f826fbf60278ed2c4d8b83479026a54dbeb
Greg Hawkins <github@greghawkins.net>
2012-12-27 13:46:40 +0000

    Command line disassembler now working.

--- src/t3chnique/main.clj
+++ src/t3chnique/main.clj
@@ -32,8 +32,8 @@
   (println "=======================================")
   (pp/pprint m))
 
-(defn format-op [{:keys [offset mnemonic args]}]
-  (format "%8d  %s %s" offset mnemonic (apply str args)))
+(defn format-op [{:keys [address offset mnemonic args]}]
+  (format "%8d %8d  %s %s" address offset mnemonic (apply str args)))
 
 (defn entp
   "Output entry point information from image"
@@ -41,53 +41,61 @@
   (let [e (first (filter #(= (:id %) "ENTP") image))]
     (output "ENTP Block" e)))
 
-(defn dis1
+(defn- dis1
   "Disassemble single instruction from buffer, incrementing pointer."
-  ([buf]
-     (let [offs (.position buf)]
-       (dis1 buf offs)))
-  ([buf offs]
-     (let [_ (.position buf offs)
-           opcode (ber/read-ubyte buf)]
-       (when-let [op (@vm/table opcode)]
-         {:offset offs
-          :opcode opcode
-          :mnemonic (vm/mnemonic op)
-          :args (ber/parse (vm/parse-spec op) buf)}))))
+  [buffer-addr method-addr buf ptr]
+  (let [_ (.position buf ptr)
+        opcode (ber/read-ubyte buf)]
+    (when-let [op (@vm/table opcode)]
+      [{:address (+ buffer-addr ptr)
+        :offset (- (+ buffer-addr ptr) method-addr)
+        :opcode opcode
+        :mnemonic (vm/mnemonic op)
+        :args (ber/parse (vm/parse-spec op) buf)}
+       (.position buf)])))
 
-(defn disn
-  [buf start end]
-  (do
-    (.position buf start)
-    (loop [ops []]
-      (if (or (zero? end) (< (.position buf) end))
-        (if-let [d (dis1 buf)]
-          (recur (conj ops d))
-          ops)
-        ops))))
+(defn dis-method
+  [buffer-addr method-addr buf start end]
+  (loop [ops [] ptr start]
+    (if (or (zero? end) (< (.position buf) end))
+      (if-let [[d nextptr] (dis1 buffer-addr method-addr buf ptr)]
+        (recur (conj ops d) nextptr)
+        ops)
+      ops)))
 
 (defn dis
   "Print disassembled code at offset"
   [offs state]
   (let [[mh s] ((vm/get-method-header offs) state)
-        code (+ offs (:code-offset mh))
-        {:keys [etable-offset dtable-offset]} mh
-        etable (if (pos? etable-offset) (+ offs etable-offset) 0)
-        dtable (if (pos? dtable-offset) (+ offs dtable-offset) 0)
-        end (if (zero? etable) dtable etable)]
-    (println "Method\n")
-    (println (format "Offsets (code/etable/dtable): %d/%d/%d\n" code etable dtable))
+        {:keys [code-offset etable-offset dtable-offset]} mh
+        code-addr (+ offs code-offset)
+        etable-addr (if (pos? etable-offset) (+ offs etable-offset) 0)
+        dtable-addr (if (pos? dtable-offset) (+ offs dtable-offset) 0)
+        end (if (zero? etable-addr) dtable-addr etable-addr)]
+
+    ;; method header
+    (println (format "Method Header @ %d\n" offs))
+    (println (format "Offsets (code/etable/dtable): %d/%d/%d\n" code-offset etable-offset dtable-offset))
     (println (format "Locals: %d Slots: %d Params: %d Opt Params: %d\n"
                      (:local-variable-count mh)
                      (:max-slots mh)
                      (:param-count mh)
                      (:opt-param-count mh)))
-    (doseq [op (apply disn (conj (vm/offset state code) end))]
-      (println (format-op op)))
-    (when (pos? etable)
-      (println)
-      (let [[b o] (vm/offset state etable)
-            _ (.position b 0)
+
+    ;; disassembled operations
+    (let [method-addr offs
+          [buf ptr] (vm/offset state code-addr)
+          buffer-addr (- code-addr ptr)
+          [_ endptr] (vm/offset state end)]
+      (println (format "Code @ %d\n" code-addr))
+      (doseq [op (dis-method buffer-addr method-addr buf ptr endptr)]
+        (println (format-op op))))
+
+    ;; exception table
+    (when (pos? etable-offset)
+      (println (format "\nException Table @ %d\n" etable-addr))
+      (let [[b o] (vm/offset state etable-addr)
+            _ (.position b o)
             erecords (im/read-exception-table b)]
         (doseq [e erecords]
           (println (format "%8d - %8d: oid: %d offset: %d"
