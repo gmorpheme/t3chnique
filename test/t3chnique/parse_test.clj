224c39b42ad680749bde53069147a5f024f85724
Greg Hawkins <greg@gmorpheme.net>
2013-03-07 19:28:25 +0000

    Fix tests.

--- test/t3chnique/parse_test.clj
+++ test/t3chnique/parse_test.clj
@@ -1,29 +1,40 @@
 (ns t3chnique.test.parse-test
   (:use [t3chnique.parse]
         [midje.sweet])
+  (:require [t3chnique.primitive :as prim])
   (:import [java.nio.charset Charset]
            [java.nio ByteOrder MappedByteBuffer ByteBuffer]))
 
-(defn init [buf] {:parse-state [buf 0]})
+(fact
+  (parse (utf8 5) (utf8-buf "hello")) => "hello")
 
-(defn make-buf [bytes]
-  (ByteBuffer/wrap
-   (byte-array
-    (map unchecked-byte bytes))))
+(fact
+  (parse (prefixed-utf8) (utf8-pbuf 5 "hello")) => "hello")
 
-(defn utf8-buf [str]
-  (make-buf (seq (.getBytes str "utf-8"))))
+(fact
+  (parse (times 4 (ubyte)) (make-buf [0x02 0x01 0x02 0x01])) => [2 1 2 1])
 
-(defn utf8-pbuf [c str]
-  (make-buf (cons c (seq (.getBytes str "utf-8")))))
+(fact
+  (parse (tagged-parser :uint2) (make-buf [0xcc 0xcc])) => 52428)
 
-(facts
-  (parse (utf8 5) (utf8-buf "hello")) => "hello"
-  (parse (prefixed-utf8) (utf8-pbuf 5 "hello")) => "hello"
-  (parse (times 4 (ubyte)) (make-buf [0x02 0x01 0x02 0x02]) => [2 1 2 1])
-  (parse (tagged-parser :uint2) (make-buf [0xcc 0xcc])) => 52428
-  (parse (data-holder) (make-buf [prim/vm-prop-id 0xcc 0xcc])) => {:type vm-prop-id, :value 52428}
-  (parse (lst) (make-buf [0x00 0x02 prim/vm-prop-id 0xbb 0xbb prim/vm-prop-id 0xcc 0xcc])) => [{:type 6, :value 48059} {:type vm-prop-id, :value 52428}]
-  (parse (within 4 (uint2)) (make-buf [1 2 3 4])) => 258
-  (second (second ((within 4 (uint2)) (init (make-buf [1 2 3 4]))))) => 4
-  (parse (parse-until zero? (ubyte)) (make-buf [1 1 1 1 0 2 2])) => [1 1 1 1 0])
+(fact
+  (parse (data-holder) (make-buf [prim/vm-prop-id 0xcc 0xcc])) => {:type prim/vm-prop-id, :value 52428})
+
+(fact
+  (parse (lst) (make-buf [0x00 0x02 prim/vm-prop-id 0xbb 0xbb 0x00 0x00 prim/vm-prop-id 0xcc 0xcc 0x00 0x00]))
+  => [{:type prim/vm-prop-id, :value 48059} {:type prim/vm-prop-id, :value 52428}])
+
+(fact
+  (parse (within 4 (uint2)) (make-buf [1 2 3 4])) => 258)
+
+(fact
+  (second (second ((within 4 (uint2)) (init-state (make-buf [1 2 3 4]))))) => 4)
+
+(fact
+  (parse (parse-until zero? (ubyte)) (make-buf [1 1 1 1 0 2 2])) => [1 1 1 1 0]) 
+
+(fact
+ (parse-resource "Elysium.t3") => #(not (empty? %)))
+
+(fact
+ (parse-resource "ditch3.t3") => #(not (empty? %)))
