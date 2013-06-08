dfbe94c39ac1ff48dafa13efff54c0bfacbfa82a
Greg Hawkins <github@greghawkins.net>
2013-06-08 11:41:58 +0100

    Dev profile to include t3 files.

--- project.clj
+++ project.clj
@@ -7,13 +7,17 @@
   :main t3chnique.main
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/algo.monads "0.1.0"]
-                 [org.clojure/tools.trace "0.7.5"]
                  [org.clojure/tools.cli "0.2.2"]
                  [org.clojure/tools.logging "0.2.6"]
                  [compojure "1.1.5"]
                  [ring/ring-jetty-adapter "1.1.0"]
                  [ring-middleware-format "0.3.0"]
                  [nio "0.0.5"]
-                 [hiccup "1.0.3"]
-                 [midje "1.5.1"]]
-  :jvm-opts ["-Xms48m" "-Xmx1024m" "-XX:MaxPermSize=512m"])
+                 [hiccup "1.0.3"]]
+  :jvm-opts ["-Xms48m" "-Xmx1024m" "-XX:MaxPermSize=512m"]
+
+  ; add t3 tests to resources for 
+  :profiles {:dev {:source-paths ["dev"]
+                   :resource-paths ["t3"]
+                   :dependencies [[org.clojure/tools.trace "0.7.5"]
+                                  [midje "1.5.1"]]}})
