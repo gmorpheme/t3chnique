7be6eb1e7b7ba52654e7f1a09d63529c96015b1e
Greg Hawkins <github@greghawkins.net>
2013-11-15 15:23:22 +0000

    Upgrade deps.

--- project.clj
+++ project.clj
@@ -6,23 +6,24 @@
   :resources-path "resources"
   :main t3chnique.main
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/algo.monads "0.1.0"]
-                 [org.clojure/tools.cli "0.2.2"]
-                 [org.clojure/java.classpath "0.2.0"]
+                 [org.clojure/algo.monads "0.1.4"]
+                 [org.clojure/tools.cli "0.2.4"]
+                 [org.clojure/java.classpath "0.2.1"]
                  [org.clojure/tools.logging "0.2.6"]
-                 [compojure "1.1.5"]
-                 [ring/ring-jetty-adapter "1.1.0"]
-                 [ring-middleware-format "0.3.0"]
-                 [nio "0.0.5"]
-                 [hiccup "1.0.3"]]
+                 [compojure "1.1.6"]
+                 [ring/ring-jetty-adapter "1.2.1"]
+                 [ring-middleware-format "0.3.1"]
+                 [nio "1.0.1"]
+                 [hiccup "1.0.4"]]
   :jvm-opts ["-Xms48m" "-Xmx1g" "-XX:MaxPermSize=512M"]
 
   :repositories [["gmorpheme-snapshots" {:url "http://dev.gmorpheme.net/artifactory/libs-snapshot"}]]
   :deploy-repositories [["gmorpheme-snapshots" {:url "http://dev.gmorpheme.net/artifactory/libs-snapshot-local"}]]
   
   ; add t3 tests to resources for 
+
   :profiles {:dev {:source-paths ["dev"]
                    :resource-paths ["t3"]
-                   :dependencies [[org.clojure/tools.trace "0.7.5"]
-                                  [org.clojure/tools.namespace "0.2.3"]
+                   :dependencies [[org.clojure/tools.trace "0.7.6"]
+                                  [org.clojure/tools.namespace "0.2.4"]
                                   [midje "1.5.1"]]}})
