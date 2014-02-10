80b1f5e1da6a2b3e3ffbe7583246cb5abca8ea4d
Greg Hawkins <github@greghawkins.net>
2014-02-10 15:19:50 +0000

    Routes context-relative for use by t3chnique.web.

--- src/t3chnique/server.clj
+++ src/t3chnique/server.clj
@@ -19,6 +19,7 @@
             [t3chnique.primitive :as p]
             [t3chnique.parse :as parse]
             [clojure.stacktrace :as st]
+            [clojure.tools.logging :refer (info debug trace)]
             [t3chnique.all :refer [default-host]]))
 
 (defn vm-actions
@@ -27,11 +28,13 @@
   (if (zero? (:ip vm)) [:action/enter] [:action/step :action/run :action/back]))
 
 (defn render-html [data]
-  (if-let [page (:page (meta data))]
-    ((resolve (symbol page)) data)
+  (trace "render-html")
+  (if-let [{:keys [page context]} (meta data)]
+    ((resolve (symbol page)) context data)
     (hp/html5 (pr-str data))))
 
 (defn render-edn [data]
+  (trace "render-edn")
   (binding [*print-dup* true]
     (pr-str data)))
 
@@ -57,61 +60,71 @@
       (update-in [:ip] p/vm-codeptr)
       (update-in [:fp] p/vm-stack)))
 
-(defn represent-game [game]
-  (assoc game :_links {:self {:href (str "/games/" (:id game))}}))
+(defn url
+  "Form a URL with the supplied context root and path elements. context should
+start with a /. Use / for the root context."
+  [context & components]
+  (string/join "/" (if (string/blank? context) components (cons context components)) ))
 
-(defn represent-game-list [gs]
-  (map represent-game gs))
+(defn represent-game [context game]
+  (assoc game :_links {:self {:href (url context "games" (:id game))}
+                       :launch {:href (url context "vms")}}))
+
+(defn represent-game-list [context gs]
+  {:games (map (partial represent-game context) gs)})
 
 (defn add-vm-links
-  [id actions repn]
-  (let [basic {:self {:href (str "/vms/" id)}
-               :stack {:href (str "/vms/" id "/stack")}
-               :registers {:href (str "/vms/" id "/registers")}
-               :exec {:href (str "/exec/" id)}
-               :code {:href (str "/vms/" id "/code{?address,length}") :templated true}
-               :const {:href (str "/vms/" id "/const{?address,length") :templated true}
-               :objects {:href (str "/vms/" id "/objects{?oid,count}") :templated true}
-               :mcld {:href (str "/vms/" id "/mcld")}
-               :fnsd {:href (str "/vms/" id "/fnsd")}
-               :symd {:href (str "/vms/" id "/symd")}
-               :exc {:href (str "/vms/" id "/exc")}
-               :dis1 {:href (str "/vms/" id "/dis1/{address}") :templated true}}
-        action-links (map (fn [x] {x {:href (str "/vms/" id "/" (name x))
+  [context id actions repn]
+  (let [basic {:self {:href (url context "vms" id)}
+               :stack {:href (url context "vms" id "stack")}
+               :registers {:href (url context "vms" id "registers")}
+               :exec {:href (url context "exec" id)}
+               :code {:href (url context "vms" id "code{?address,length}") :templated true}
+               :const {:href (url context "vms" id "const{?address,length}") :templated true}
+               :objects {:href (url context "vms" id "objects{?oid,count}") :templated true}
+               :mcld {:href (url context "vms" id "mcld")}
+               :fnsd {:href (url context "vms" id "fnsd")}
+               :symd {:href (url context "vms" id "symd")}
+               :exc {:href (url context "vms" id "exc")}
+               :dis1 {:href (url context "vms" id "dis1" "{address}") :templated true}}
+        action-links (map (fn [x] {x {:href (url context "vms" id (name x))
                                      :name (string/capitalize (name x))}})
                           actions)
         links (reduce merge basic action-links)]
     (assoc repn :_links links)))
 
-(defn represent-vm [id vm]
-  (add-vm-links id (vm-actions vm) {:id id :sequence (:sequence vm)}))
+(defn represent-vm [context id vm]
+  (add-vm-links context id (vm-actions vm) {:id id :sequence (:sequence vm)}))
 
-(defn represent-vm-stack [id vm]
-  (add-vm-links id (vm-actions vm) (select-keys vm [:stack])))
+(defn represent-vm-stack [context id vm]
+  (add-vm-links context id (vm-actions vm) (select-keys vm [:stack])))
 
-(defn represent-vm-registers [id vm]
-  (add-vm-links id
+(defn represent-vm-registers [context id vm]
+  (add-vm-links context
+                id
                 (vm-actions vm)
                 (promote-types (select-keys vm [:r0 :ip :ep :sp :fp :say-function :say-method]))))
 
-(defn represent-vm-map [vms]
+(defn represent-vm-map [context vms]
   (for [[id vm] vms]
-    (add-vm-links id (vm-actions vm) {:id id :sequence (:sequence vm)})))
+    (add-vm-links context id (vm-actions vm) {:id id :sequence (:sequence vm)})))
 
 (defn ubytes [page offset len]
   (map #(bit-and 0xff %) (parse/read-bytes page offset len)))
 
-(defn represent-vm-code [id vm addr len]
+(defn represent-vm-code [context id vm addr len]
   (let [[p off] (t3vm/offset vm addr)]
     (add-vm-links
+     context
      id
      (vm-actions vm)
      {:id id :code-section {:address addr :bytes (ubytes p off len)}})))
 
-(defn represent-vm-assembly [id vm addr {:keys [op args]}]
+(defn represent-vm-assembly [context id vm addr {:keys [op args]}]
   (let [doc-url (str "http://www.tads.org/t3doc/doc/techman/t3spec/opcode.htm#opc_" (.toLowerCase (name (:mnemonic op))))
         op (dissoc op :run-fn)]
-    (add-vm-links id
+    (add-vm-links context
+                  id
                   (vm-actions vm)
                   {:id id
                    :assembly {:op op
@@ -119,15 +132,17 @@
                               :address addr
                               :_links {:doc {:href doc-url}}}})))
 
-(defn represent-vm-const [id vm addr len]
+(defn represent-vm-const [context id vm addr len]
   (let [[p off] (t3vm/const-offset vm addr)]
     (add-vm-links
+     context
      id
      (vm-actions vm)
      {:id id :const-section {:address addr :bytes (ubytes p off len)}})))
 
-(defn represent-vm-objects [id vm oid count]
+(defn represent-vm-objects [context id vm oid count]
   (add-vm-links
+   context
    id
    (vm-actions vm)
    {:id id
@@ -135,42 +150,43 @@
            (fn [[k v]] {:oid (p/vm-obj k) :value v})
            (take count (subseq (:objs vm) >= oid)))}))
 
-(defn represent-vm-mcld [id vm]
+(defn represent-vm-mcld [context id vm]
   (add-vm-links
+   context
    id
    (vm-actions vm)
    {:id id
     :mcld (map #(dissoc % :metaclass :_prototype) (:mcld vm))}))
 
-(defn represent-vm-fnsd [id vm]
+(defn represent-vm-fnsd [context id vm]
   (add-vm-links
+   context
    id
    (vm-actions vm)
    {:id id
     :fnsd (:fnsd vm)}))
 
-(defn represent-vm-symd [id vm]
+(defn represent-vm-symd [context id vm]
   (add-vm-links
+   context
    id
    (vm-actions vm)
    {:id id
     :symd (:symd vm)}))
 
-(defn represent-vm-exc [id vm]
+(defn represent-vm-exc [context id vm]
   (add-vm-links
+   context
    id
    (vm-actions vm)
    {:id id
     :exc (or (:exc vm) nil)}))
 
 (defn respond
-  ([data]
+  ([context data]
      {:body data})
-  ([page data]
-     {:body (with-meta data {:page page})}))
-
-
-
+  ([context page data]
+     {:body (with-meta data {:page page :context context})}))
 
 ;; tooling site - static html access to the restful data
 
@@ -185,49 +201,62 @@
      [:script {:src "http://underscorejs.org/underscore-min.js"}]
      body]]))
 
-(defn rest-chrome [nav-name body]
+(defn nav-data [context selected]
+  {:selected selected
+   :items [{:name "games"
+            :href (url context "games")
+            :text "Games"}
+           {:name "vms"
+            :href (url context "vms")
+            :text "VMs"}]})
+
+(defn rest-chrome [{:keys [selected items] :as nav-data} body]
   (chrome
    [:ul.nav.nav-tabs
-    [:li {:class (if (= nav-name "games") "active" "inactive")} [:a {:href "/games"} "Games"]]
-    [:li {:class (if (= nav-name "vms") "active" "inactive")} [:a {:href "/vms"} "VMs"]]]
+    (for [{:keys [name href text]} items]
+      [:li {:class (if (= selected name) "active" "inactive")} [:a {:href href} text]])]
    [:div.tab-content
     body]))
 
-(defn games-page [games]
-  (rest-chrome "games" [:div#games.row
-                           [:div.span4
-                            [:ul.nav.nav-list
-                             (for [game games]
-                               [:li [:a {:href (str "/games/" (:id game)) } (:name game)]])]]]))
+(defn games-page [context {:keys [games] :as game-list}]
+  (rest-chrome (nav-data context "games")
+               [:div#games.row
+                [:div.span4
+                 [:ul.nav.nav-list
+                  (for [game games]
+                    [:li [:a {:href (get-in game [:_links :self :href]) } (:name game)]])]]]))
 
-(defn game-page [game]
-  (rest-chrome "games" [:div#game
-                           [:div.span4
-                            [:h2 (:name game)]
-                            [:form {:method "post" :action "/vms"}
-                             [:input {:type "hidden" :name "game" :value (:id game)}]
-                             [:button.btn {:type "submit"} "Launch"]]]] ))
+(defn game-page [context game]
+  (rest-chrome (nav-data context "games")
+               [:div#game
+                [:div.span4
+                 [:h2 (:name game)]
+                 [:form {:method "post" :action (get-in game [:_links :launch :href])}
+                  [:input {:type "hidden" :name "game" :value (:id game)}]
+                  [:button.btn {:type "submit"} "Launch"]]]] ))
 
-(defn vms-page [vms]
-  (rest-chrome "vms" [:div#vms.row
-                         [:dev.span4
-                          [:ul.nav.nav-list
-                           (for [{:keys [id _links]} vms]
-                             [:li id
-                              (for [[rel attrs] _links]
-                                [:a (assoc attrs :rel rel) rel])])]]]))
+(defn vms-page [context vms]
+  (rest-chrome (nav-data context "vms")
+               [:div#vms.row
+                [:dev.span4
+                 [:ul.nav.nav-list
+                  (for [{:keys [id _links]} vms]
+                    [:li id
+                     (for [[rel attrs] _links]
+                       [:a (assoc attrs :rel rel) rel])])]]]))
 
-(defn vm-page [{:keys [id _links]}]
-  (rest-chrome "vms" [:div#vms.row
-                         [:dev.span4
-                          [:ul.nav.nav-list
-                           [:span id]
-                           (for [[rel attrs] _links]
-                             [:a (assoc attrs :rel rel) rel])]]]))
+(defn vm-page [context {:keys [id _links]}]
+  (rest-chrome (nav-data context "vms")
+               [:div#vms.row
+                [:dev.span4
+                 [:ul.nav.nav-list
+                  [:span id]
+                  (for [[rel attrs] _links]
+                    [:a (assoc attrs :rel rel) rel])]]]))
 
-(defn exec-page [{:keys [id]}]
+(defn exec-page [context {:keys [id]}]
   (chrome
-   [:script (str "vm_url = '/exec/" id  "';")]
+   [:script (str "vm_url = '" (url context "exec" id)  "';")]
    [:div {:id "header"}]
    [:div {:id "information" :clear "left" :width "100%"}]
    [:div {:id "controls" :clear "left" :width "100%"}]
@@ -292,6 +321,7 @@
         id (first (remove (partial contains? @histories) (repeatedly #(new-id prefix))))
         vm (assoc (t3vm/vm-from-image (parse/parse-resource name)) :id id)
         host (default-host)]
+    (info "Created VM:" id)
     (swap! histories assoc id [vm])
     (swap! hosts assoc id host)
     vm))
@@ -352,120 +382,125 @@
 
 (defn make-routes
   "Make routes for system"
-  [system]
+  [context system]
   {:pre [(not (nil? system))
          (associative? system)]}
   (routes
 
    (GET "/games" []
-     (respond "t3chnique.server/games-page" (represent-game-list (game-list system))))
+     (respond context "t3chnique.server/games-page" (represent-game-list context (game-list system))))
 
    (GET ["/games/:id"] [id]
      (let [id (Integer/parseInt id)]
        (if-let [game (game-get system id)]
-         (respond "t3chnique.server/game-page" (represent-game game))
+         (respond context "t3chnique.server/game-page" (represent-game context game))
          (response/not-found "Nonesuch"))))
 
    (GET "/vms" []
-     (respond "t3chnique.server/vms-page" (represent-vm-map (vm-map system))))
+     (respond context "t3chnique.server/vms-page" (represent-vm-map context (vm-map system))))
 
    (GET ["/vms/:id"] [id]
      (if-let [vm (vm-get system id)]
-       (respond "t3chnique.server/vm-page" (represent-vm id vm))
+       (respond context "t3chnique.server/vm-page" (represent-vm context id vm))
        (response/not-found "Nonesuch")))
    
    (GET ["/vms/:id/stack"] [id]
      (if-let [vm (vm-get system id)]
-       (respond (represent-vm-stack id vm))
+       (respond context (represent-vm-stack context id vm))
        (response/not-found "Nonesuch")))
    
    (GET ["/vms/:id/registers"] [id]
      (if-let [vm (vm-get system id)]
-       (respond (represent-vm-registers id vm))
+       (respond context (represent-vm-registers context id vm))
        (response/not-found "Nonesuch")))
    
    (GET ["/vms/:id/code"] [id address length]
      (let [addr (Integer/parseInt address)
            len (Integer/parseInt length)]
        (if-let [vm (vm-get system id)]
-         (respond (represent-vm-code id vm addr len))
+         (respond context (represent-vm-code context id vm addr len))
          (response/not-found "Nonesuch"))))
 
    (GET ["/vms/:id/const"] [id address length]
      (let [addr (Integer/parseInt address)
            len (Integer/parseInt length)]
        (if-let [vm (vm-get system id)]
-         (respond (represent-vm-const id vm addr len))
+         (respond context (represent-vm-const context id vm addr len))
          (response/not-found "Nonesuch"))))
 
    (GET ["/vms/:id/objects"] [id oid count]
      (let [o (Integer/parseInt oid)
            n (Integer/parseInt count)]
        (if-let [vm (vm-get system id)]
-         (respond (represent-vm-objects id vm o n))
+         (respond context (represent-vm-objects context id vm o n))
          (response/not-found "Nonesuch"))))
    
    (GET ["/vms/:id/mcld"] [id]
      (if-let [vm (vm-get system id)]
-       (respond (represent-vm-mcld id vm))
+       (respond context (represent-vm-mcld context id vm))
        (response/not-found "Nonesuch")))
 
    (GET ["/vms/:id/fnsd"] [id]
      (if-let [vm (vm-get system id)]
-       (respond (represent-vm-fnsd id vm))
+       (respond context (represent-vm-fnsd context id vm))
        (response/not-found "Nonesuch")))
    
    (GET ["/vms/:id/symd"] [id]
      (if-let [vm (vm-get system id)]
-       (respond (represent-vm-symd id vm))
+       (respond context (represent-vm-symd context id vm))
        (response/not-found "Nonesuch")))
    
    (GET ["/vms/:id/exc"] [id]
      (if-let [vm (vm-get system id)]
-       (respond (represent-vm-exc id vm))
+       (respond context (represent-vm-exc context id vm))
        (response/not-found "Nonesuch")))
 
    ;; actions
    
    (POST ["/vms"] [game]
      (let [game (Integer/parseInt game)]
-       (response/redirect-after-post (str "/vms/" (:id (vm-new! system game))))))
+       (response/redirect-after-post (url context "vms" (:id (vm-new! system game))))))
    
    (POST ["/vms/:id/step"] [id]
-     (respond (represent-vm id (vm-step! system id))))
+     (respond context (represent-vm context id (vm-step! system id))))
    
    (POST ["/vms/:id/enter"] [id]
-     (respond (represent-vm id (vm-enter! system id))))
+     (respond context (represent-vm context id (vm-enter! system id))))
    
    (POST ["/vms/:id/run"] [id]
-     (respond (represent-vm id (vm-run! system id))))
+     (respond context (represent-vm context id (vm-run! system id))))
 
    (POST ["/vms/:id/back"] [id]
-     (respond (represent-vm id (vm-back! system id))))
+     (respond context (represent-vm context id (vm-back! system id))))
    
    (GET ["/vms/:id/dis1/:addr"] [id addr]
      (let [addr (Integer/parseInt addr)]
        (if-let [vm (vm-get system id)]
-         (respond (represent-vm-assembly id (vm-get system id) addr (dis1 system id addr)))
+         (respond context (represent-vm-assembly context id (vm-get system id) addr (dis1 system id addr)))
          (response/not-found "Nonesuch"))))
    
    (GET ["/exec/:id"] [id]
      (if-let [vm (vm-get system id)]
-       (respond "t3chnique.server/exec-page" (represent-vm id vm))
+       (respond context "t3chnique.server/exec-page" (represent-vm context id vm))
        (response/not-found "Nonesuch")))
    
    (route/resources "/")
    
    (route/not-found "No such resource")))
 
-(defn make-app [system]
-  (-> (handler/api (make-routes system))
+(defn make-app
+  "Make a Ring app for serving the REST tools API. Context root must be
+supplied for URL formation."
+  [context system]
+  (-> (handler/api (make-routes context system))
       (wrap-restful-params)
       (wrap-response)
       (wrap-stacktrace-web)))
 
-(defn start-server [system]
-  (j/run-jetty (make-app system) {:port 8080 :join? false}))
+(defn start-server
+  "Start server with REST API at root context."
+  [system]
+  (j/run-jetty (make-app "/" system) {:port 8080 :join? false}))
 
 (defn stop-server [system]
   (when-let [server @(:server system)]
